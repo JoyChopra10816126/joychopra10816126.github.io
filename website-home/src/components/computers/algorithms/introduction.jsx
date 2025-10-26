0e4185470eca73e2db854acb4824b9399f5a94fd
Joy Chopra <joy.chopra2@gmail.com>
2025-10-26 16:50:24 +0530

    Introduction to algorithms - formal

--- website-home/src/components/computers/algorithms/introduction.jsx
+++ website-home/src/components/computers/algorithms/introduction.jsx
@@ -1,7 +1,51 @@
 import { Container, Header } from "@cloudscape-design/components";
 import ModeSelector from "../../mode-selector/mode-selector";
+import { useMode } from "../../mode-selector/mode-context";
+import MathText from "../../latex/math-text";
 
 const AlgorithmsIntroduction = () => {
+    const { learningMode } = useMode();
+    let content = "";
+    if (learningMode) { 
+      content = (<MathText>
+        Let {"\\( S \\)"} be the set of all states of a computational system {"\\( C \\)"}.
+        <br/>
+
+        Let {"\\( I \\subseteq S \\)"} be the set of input states
+        <br/>
+        
+        Let {"\\( O \\subseteq S \\)"} be the set of output states
+        <br/><br/> 
+
+      An <b>algorithm</b> {"\\( A \\)"} can be defined as a finite ordered sequence of 
+      well-defined computational steps.
+
+      {"$$ A = (\\delta_1, \\delta_2, \\dots, \\delta_n), $$"}
+      where {"\\( \\delta_i : S \\to S\\)"} are the {"\\( n \\)"} <b>computational steps</b> of the algorithm.
+      <br/>
+
+      Each computational step transitions the computational system from one state to another.
+      <br/><br/>
+
+      On <b>finiteness</b> - The number of computational steps is a natural number,  
+      &nbsp;{"\\( n \\in \\mathbb{N}, n \\lt \\infty  \\)"}
+      <br/><br/>
+
+      On <b>order</b> - Each computational step is applied in order as described below.
+      Let {"\\( s_i \\)"} denote the state of {"\\( C \\)"} after {"\\( i \\)"}th computational step is executed.
+      Further, let {"\\( s_0 \\in I \\)"} be the initial computational state
+       and let {"\\( s_n \\in O \\)"} be the final computational state after execution of the algorithm
+
+      Then, we have
+
+      {"$$ s_{i + 1} = \\delta_i (s_i), \\;\\; i = 0, 1, 2 \\dots, n - 1 $$"}
+
+      </MathText>
+      );
+    }
+    else {
+      content = `Beginner`
+    }
   return (
     <div className="content">
       <Container
@@ -14,7 +58,9 @@ const AlgorithmsIntroduction = () => {
           </Header>
         }
       >
-
+        <div>
+        {content}
+        </div>
       </Container>
     </div>
   )
---
+++ website-home/src/components/latex/math-text.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from "react";
+
+export default function MathText({ children }) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const renderMath = () => {
+      if (window.MathJax && containerRef.current) {
+        window.MathJax.typesetPromise([containerRef.current]);
+      }
+    };
+    renderMath();
+  }, [children]); // re-typeset on content change
+
+  return <span><div ref={containerRef}>{children}</div></span>;
+}
