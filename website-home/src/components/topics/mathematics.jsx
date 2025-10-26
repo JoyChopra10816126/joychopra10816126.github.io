7af31b60204134de11defb6ac6597bcb9504eec4
Joy Chopra <joy.chopra2@gmail.com>
2025-10-26 09:28:10 +0530

    Added all mathematics topics

--- website-home/src/components/topics/mathematics.jsx
+++ website-home/src/components/topics/mathematics.jsx
@@ -24,15 +24,60 @@ const Mathematics = () => {
         ]}
         items={[
           {
-            name: "Arithmetic",
-            description: "Basic operations with numbers",
-            url: '/mathematics/arithmetic'
-          },
-          {
             name: "Algebra",
             description: "Symbols and equations for relations",
             url: '/mathematics/algebra'
           },
+          {
+            name: "Calculus",
+            description: "Study of change and motion",
+            url: '/mathematics/calculus'
+          },
+          {
+            name: "Complex Analysis",
+            description: "Study of complex-valued functions",
+            url: "/mathematics/complex-analysis"
+          },
+          {
+            name: "Functional Analysis",
+            description: "Study of function spaces and operators",
+            url: "/mathematics/functional-analysis"
+          },
+          {
+            name: "Linear Algebra",
+            description: "Study of vectors and transformations",
+            url: "/mathematics/linear-algebra"
+          },
+          {
+            name: "Linear Programming",
+            description: "Study of linear optimization problems",
+            url: "/mathematics/linear-programming"
+          },
+          {
+            name: "Numerical Analysis",
+            description: "Study of computational approximation methods",
+            url: "/mathematics/numerical-analysis"
+          },
+          {
+            name: "Ordinary Differential Equations",
+            description: "Study of single-variable derivatives",
+            url: "/mathematics/ordinary-differential-equations"
+          },
+          {
+            name: "Partial Differential Equations",
+            description: "Study of multi-variable derivatives",
+            url: "/mathematics/partial-differential-equations"
+          },
+          {
+            name: "Real Analysis",
+            description: "Study of real number functions",
+            url: "/mathematics/real-analysis"
+          },
+          {
+            name: "Topology",
+            description: "Study of space and continuity",
+            url: "/mathematics/topology"
+          }
         ]}
       />
     </div>
