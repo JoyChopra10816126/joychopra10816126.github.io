b2aed84532d35c1df5fba3b60c959a57957df5c3
Joy Chopra <joy.chopra2@gmail.com>
2025-11-15 14:12:45 +0530

    Added examples for time complexity article

--- website-home/src/components/computers/algorithms/time-complexity.jsx
+++ website-home/src/components/computers/algorithms/time-complexity.jsx
@@ -52,14 +52,10 @@ const TimeComplexity = () => {
         <li><MathText>{"\\( O(c.f(n)) = O(f(n)) \\)"}</MathText> where c is a natural number</li>
         <li><MathText>{"\\( O(f_1(n) + f_2(n)) = O(f_1(n)) \\; \\text{if}" + 
         "\\; \\; \\forall i \\in \\mathbb{N}, \\; f_1(i) \\geq f_2(i)\\)"}</MathText></li>
-        <li><MathText>{"\\( \\Omega(c.f(n)) = \\Omega(f(n)) \\)"}</MathText> where c is a natural number</li>
-        <li><MathText>{"\\( \\Omega(f_1(n) + f_2(n)) = \\Omega(f_1(n)) \\; \\text{if}" + 
-        "\\; \\; \\forall i \\in \\mathbb{N}, \\; f_1(i) \\geq f_2(i)\\)"}</MathText></li>
-        <li><MathText>{"\\( \\theta(c.f(n)) = \\theta(f(n)) \\)"}</MathText> where c is a natural number</li>
-        <li><MathText>{"\\( \\theta(f_1(n) + f_2(n)) = \\theta(f_1(n)) \\; \\text{if}" + 
-        "\\; \\; \\forall i \\in \\mathbb{N}, \\; f_1(i) \\geq f_2(i)\\)"}</MathText></li>
-        
+        <li><MathText>{"\\( O(f_1(n)) + O(f_2(n)) = O(f_1(n) + f_2(n)) \\)"}</MathText></li>
       </ol>
+      Similar rules hold true for <MathText>{"\\( \\Omega(n) \\)"}</MathText>
+      &nbsp;and <MathText>{"\\( \\theta(n) \\)"}</MathText>
     </div>
     );
   }
@@ -83,22 +79,46 @@ const TimeComplexity = () => {
         <b>Example 1</b>
         <br />
         Consider the following algorithm.
-        Input - An array a[i] of n elements
+        <br/>
+        <b>Input</b> - An array a[i] of n elements
+        <br/>
         Step 1 - Initialise a loop variable i = 0
+        <br/>
         Step 2 - Until i = n - 1, do the following
-          Step 2.1 - a[i] = 2*a[i]
-          Step 2.2 - i = i + 1
-        Output - array a[i] with updated values
+        <br/>
+        &nbsp;&nbsp;Step 2.1 - a[i] = 2 * a[i]
+        <br/>
+        &nbsp;&nbsp;Step 2.2 - i = i + 1
+        <br/>
+        <b>Output</b> - array a[i] with updated values
+        <br/><br/>
 
         Let us analyse the time complexity for above algorithm.
+        <br/>
         Step 1 does one assignment operation. This is independent of input size.
-        Hence it takes O(1) or constant time.
+        Hence it takes <MathText>{"\\( T_{\\delta_1}(n) = O(1) \\)"}</MathText> or constant time.
+        <br/>
         Step 2 is a loop. Inside the loop, step 2.1 and 2.2 are again assignment operations.
         So we have 2 operations for one iteration of loop.
         The loop runs from i = 0 till i = n - 1.
         So the loop performs n iterations. Each iteration executes 2 operations.
-        Hence step 2 take O(2n) = O(n) = Linear time. 
+        Hence step 2 takes <MathText>{"\\( T_{\\delta_2}(n) = O(2n) \\)"}</MathText> or linear time
+        <br/>
+        Overall time taken by the algorithm A is as follows 
+        <br/>
+        <MathText>{"$$ T_A(n) = T_{\\delta_1}(n) + T_{\\delta_2}(n)$$"}</MathText>
+        <MathText>{"$$ = O(2n) + O(1) $$"}</MathText>
+        <MathText>{"$$ = O(2n + 1) = O(n) $$"}</MathText>
 
+        <b>Example 2</b>
+        <br />
+        Let us now go through examples on simplification
+        <br/>
+        <ol class="spaced-list">
+        <li><MathText>{"\\( O(100) = O(1) \\)"}</MathText></li>
+        <li><MathText>{"\\( O(n^2 + 50n + 5) = O(n^2) \\)"}</MathText></li>
+        <li><MathText>{"\\( O(100n) + O(300n) = O(n) \\)"}</MathText></li>
+        </ol>
       </div>
     )
   }
