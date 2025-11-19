ef708d22dca3b8a8631d37db6e57d5cae2f34149
Joy Chopra <joy.chopra2@gmail.com>
2025-11-19 17:24:56 +0530

    Space complexity beginner article

--- website-home/src/components/computers/algorithms/space-complexity.jsx
+++ website-home/src/components/computers/algorithms/space-complexity.jsx
@@ -10,8 +10,20 @@ const SpaceComplexity = () => {
       content = "";
     // } else if (isAdvancedMode(mode)) {
 
-    // } else if (isBasicMode(mode)) {
-
+    } else if (isBasicMode(mode)) {
+      content = (
+        <div>
+          1. Space complexity measures how much extra memory an algorithm takes.
+          <br />
+          2. It includes space for variables, data structures and function call stack.
+          <br />
+          3. Space complexity is represented using Big O notation.
+          <br />
+          4. Input size is not counted in space complexity.
+          <br />
+          5. Increasing space can reduce the time taken by the algorithm.
+        </div>
+      );
     } else {
 
     }
