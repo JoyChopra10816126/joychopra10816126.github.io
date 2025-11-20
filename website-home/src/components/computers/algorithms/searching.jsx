301331b0a997f4920087a98a7e3846f68e0c6e27
Joy Chopra <joy.chopra2@gmail.com>
2025-11-21 01:17:57 +0530

    Algorithms searching examples article

--- website-home/src/components/computers/algorithms/searching.jsx
+++ website-home/src/components/computers/algorithms/searching.jsx
@@ -45,11 +45,12 @@ const Searching = () => {
       find <MathText>{"\\( s_i \\)"}</MathText> such 
       that <MathText>{"\\( P(s_i) = true \\)"}</MathText>. If we find 
       such <MathText>{"\\( s_i \\)"}</MathText>, then return it.
-      Otherwise if no element satisfies the predicat, 
+      Otherwise if no element satisfies the predicate, 
       then return <MathText>{"\\( \\bot \\)"}</MathText>
       <br/>
       Linear search takes linear time <MathText>{"\\( O(n) \\)"}</MathText>.
-      If the set is ordered and we have a monotonic predicate, then we can use
+      If the set is ordered and we have a monotonic predicate, 
+      then we can use the faster algorithm
       binary search. Binary search takes logarithmic 
       time <MathText>{"\\( O(log_2(n))\\)"}</MathText>
     </div>
@@ -57,7 +58,36 @@ const Searching = () => {
 
   const examplesContent = (
     <div>
+      <b>Example 1</b>
+      <br/>
+      The goal is to search the number 7 in following 
+      list <MathText>{"\\( S = [2, 4, 5, 7, 3, 1] \\)"}</MathText>
+      <br/>
+      The predicate for search is <MathText>{"\\( P(x) = (x = 7) \\)"}</MathText>
+      <br/>
+      Now lets see how linear search works. We iterate over the collection as described below.
+      <br/>
+      <MathText>{"$$ x = 2, P(x) = false $$"}</MathText>
+      <MathText>{"$$ x = 4, P(x) = false $$"}</MathText>
+      <MathText>{"$$ x = 5, P(x) = false $$"}</MathText>
+      <MathText>{"$$ x = 7, P(x) = true $$"}</MathText>
+      The output of the search is x = 7. The search stops after 4th iteration.
+      <br/><br/>
 
+      <b>Example 2</b>
+      <br/>
+      The goal is to find a number that is divisible by 5 in following 
+      list <MathText>{"\\( S = [42, 14, 15, 27, 13, 61] \\)"}</MathText>
+      <br/>
+      The predicate for search is <MathText>{"\\( P(x) = (x \\; \\% \\; 5 = 0) \\)"}</MathText>
+      <br/>
+      Lets see how linear search works in this case. 
+      We iterate over the collection as described below.
+      <br/>
+      <MathText>{"$$ x = 42, P(x) = false $$"}</MathText>
+      <MathText>{"$$ x = 14, P(x) = false $$"}</MathText>
+      <MathText>{"$$ x = 15, P(x) = true $$"}</MathText>
+      The output of the search is x = 15. The search stops after 3rd iteration.
     </div>
   );
 
